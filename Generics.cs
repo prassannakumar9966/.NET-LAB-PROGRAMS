619ced0a2f32e8422232c870837fd994c811fe05
prassannakumar9966 <72078810+prassannakumar9966@users.noreply.github.com>
2021-04-08 07:05:49 +0530

    Create Generics.cs

---
+++ Generics.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+    public class MyGenericArray<T>
+    {
+        private T[] array;
+
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+
+           
+            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+
+           
+            for (int c = 0; c < 5; c++)
+            {
+                intArray.setItem(c, c * 5);
+            }
+
+        
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(intArray.getItem(c) + " ");
+            }
+
+            Console.WriteLine();
+
+         
+            MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+
+           
+            for (int c = 0; c < 5; c++)
+            {
+                charArray.setItem(c, (char)(c + 97));
+            }
+
+         
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(charArray.getItem(c) + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
