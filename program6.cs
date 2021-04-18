0622d1077db9683c28023b5b400c0b638a71d9df
prassannakumar9966 <72078810+prassannakumar9966@users.noreply.github.com>
2021-04-18 18:52:10 +0530

    Create program6.cs

---
+++ program6.cs
@@ -0,0 +1,39 @@
+# .NET-LAB-PROGRAMS
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+         string filepath = "Z:\\.NET LAB\\lab6\\ConsoleApplication22\\bin\\Debug\\lab6.txt";
+            Console.WriteLine("WRITING TO A FILE");
+                WriteToFile(filepath);
+            Console.WriteLine("READING FROM A FILE");
+                ReadFromFile(filepath);
+}public static void ReadFromFile(string filepath)
+            {
+                StreamReader reader = new StreamReader(filepath);
+                int count = 0;
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                    count++;
+                }
+                reader.Close();
+            }public static void WriteToFile(string filepath)
+            {
+                StreamWriter writer = new StreamWriter(filepath, true);
+                string temp = string.Empty;
+            Console.WriteLine("enter name[-1 to exit]:");
+            do
+                {
+                    
+                    temp = Console.ReadLine();
+                    if (temp != "-1")
+                    {
+                        writer.WriteLine(temp);
+                    }
+                } while (temp != "-1");
+                writer.Close();
+            Console.ReadLine();
+        }
